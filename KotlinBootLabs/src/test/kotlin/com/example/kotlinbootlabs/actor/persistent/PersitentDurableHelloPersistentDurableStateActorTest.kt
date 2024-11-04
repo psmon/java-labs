1abbdd09d8cc266d92c857e28f30a1ac9847ba36
psmon <psmon@live.co.kr>
2024-11-04 14:46:33 +0900

    testHelloPersistentStateActorConcurrentUsers 추가

--- KotlinBootLabs/src/test/kotlin/com/example/kotlinbootlabs/actor/persistent/PersitentDurableHelloPersistentDurableStateActorTest.kt
+++ KotlinBootLabs/src/test/kotlin/com/example/kotlinbootlabs/actor/persistent/PersitentDurableHelloPersistentDurableStateActorTest.kt
@@ -74,7 +74,7 @@ class PersitentDurableHelloPersistentDurableStateActorTest {
 
 
     @ParameterizedTest
-    @ValueSource(ints = [1, 1000])
+    @ValueSource(ints = [1000])
     fun testHelloPersistentStateActorRespondsBasedOnStatePerformTest(testCount: Int) {
 
         val probe: TestProbe<Any> = testKit.createTestProbe()
@@ -137,5 +137,52 @@ class PersitentDurableHelloPersistentDurableStateActorTest {
         helloPersistentDurableStateActor.tell(GetHelloCountPersistentDurable(probe.ref()))
         val resetResponse = probe.expectMessageClass(HelloCountResponse::class.java)
         assertEquals(0, resetResponse.count.toInt())
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = [10] )
+    fun testHelloPersistentStateActorConcurrentUsers(testCount: Int) {
+        val probe: TestProbe<Any> = testKit.createTestProbe()
+
+        val testUser: Int = 1000
+
+        val totalTestCount : Int = testCount * testUser
+
+        val actors = (1..testUser).map { i ->
+            val persistenceId = PersistenceId.ofUniqueId("HelloPersistentStateActor$i")
+            testKit.spawn(HelloPersistentDurableStateActor.create(persistenceId))
+        }
+
+        val startTime = System.currentTimeMillis()
+
+        actors.forEach { actor ->
+            actor.tell(GetHelloCountPersistentDurable(probe.ref()))
+            val response = probe.expectMessageClass(HelloCountResponse::class.java)
+            val totalCount: Number = response.count
+
+            // Test in HAPPY state
+            actor.tell(ChangeState(State.HAPPY))
+
+            for (i in 1..testCount) {
+                actor.tell(HelloPersistentDurable("Hello", probe.ref()))
+            }
+
+            for (i in 1..testCount) {
+                probe.expectMessage(HelloResponse("Kotlin"))
+            }
+
+            // Verify the hello count
+            val increaseCount: Number = totalCount.toInt() + testCount
+            actor.tell(GetHelloCountPersistentDurable(probe.ref()))
+            val updatedResponse = probe.expectMessageClass(HelloCountResponse::class.java)
+            assertEquals(increaseCount.toInt(), updatedResponse.count.toInt())
+        }
+
+        val endTime = System.currentTimeMillis()
+        val totalTime = endTime - startTime
+        val averageTimePerIteration = totalTime.toDouble() / totalTestCount
+
+        println("Total execution time: $totalTime ms")
+        println("Average execution time per iteration: $averageTimePerIteration ms")
     }
 }
